3de5afa068b6e6060d48bbcd5c1aa62f7c6d2eb6
Vamshi Muluguri <vamshimuluguri@gmail.com>
2024-05-25 12:25:28 +0530

    Update App.tsx

--- medium-blog-project/frontend/src/App.tsx
+++ medium-blog-project/frontend/src/App.tsx
@@ -9,8 +9,7 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Signin />}></Route>
+        <Routes> 
           <Route path="/signup" element={<Signup />}></Route>
           <Route path="/signin" element={<Signin />}></Route>
           <Route path="/blog/:id" element={<Blog />}></Route>
